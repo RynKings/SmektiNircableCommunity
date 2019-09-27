d3c429965c8408e0a3706342ab4563c60837f029
mrezkys <mrezkysulihin@gmail.com>
2019-09-27 11:16:13 +0800

    change /barcode-sanncer to /barcode-scanner/ngx

--- src/app/scanner/scanner.page.ts
+++ src/app/scanner/scanner.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-scanner',
@@ -10,9 +10,9 @@ export class ScannerPage implements OnInit {
   public barcode: string;
   constructor( private barcodeScanner: BarcodeScanner) { }
 
-  scan() {
+  scanCode() {
     this.barcodeScanner.scan().then(barcodeData => {
-      this.barcode = barcodeData['text'];
+      alert('Barcode data ' + JSON.stringify(barcodeData));
       console.log('Barcode Data :', barcodeData);
     }).catch( err => {
       console.log('error', err);
