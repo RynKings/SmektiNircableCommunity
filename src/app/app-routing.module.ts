97184d90be56a46e8c5c82e29c85240bc8761986
mrezkys <mrezkysulihin@gmail.com>
2019-11-08 22:30:12 +0800

    login frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'scanner', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'scanner', loadChildren: './scanner/scanner.module#ScannerPageModule' },
-  { path: 'login', loadChildren: './Auth/login/login.module#LoginPageModule' }, 
+  { path: 'login', loadChildren: './Auth/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './Auth/register/register.module#RegisterPageModule' },
   { path: 'idcard', loadChildren: './Profile/idcard/idcard.module#IdcardPageModule' },
 ];
