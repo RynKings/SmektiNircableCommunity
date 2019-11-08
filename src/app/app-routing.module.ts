2570646948d65f545a126a9173bb483ba8c5c6eb
mrezkys <mrezkysulihin@gmail.com>
2019-11-08 22:28:17 +0800

    login frontend change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'scanner', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'scanner', loadChildren: './scanner/scanner.module#ScannerPageModule' },
+  { path: 'login', loadChildren: './Auth/login/login.module#LoginPageModule' }, 
   { path: 'register', loadChildren: './Auth/register/register.module#RegisterPageModule' },
   { path: 'idcard', loadChildren: './Profile/idcard/idcard.module#IdcardPageModule' },
 ];
